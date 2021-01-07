df10ee41002ef399a3792568ef0de482765cc39b
shbhmj <35397465+shbhmj@users.noreply.github.com>
2021-01-07 23:46:31 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+// initializing variables
+$username = "";
+$email    = "";
+$errors = array(); 
+
+// connect to the database
+$db = mysqli_connect('localhost', 'dbmasteruser', '6KYY>nYf|TW}Jz5f|K#5DDIKE6B*bxm#', 'dbmaster');
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $name = mysqli_real_escape_string($db, $_POST['name']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['psw']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['psw-repeat']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM users WHERE email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO users (name, email, password) 
+  			  VALUES('$name', '$email', '$psw')";
+  	mysqli_query($db, $query);
+  	$_SESSION['name'] = $name;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: login.php');
+  }
+}
