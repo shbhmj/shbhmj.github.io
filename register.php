9d064bd114918e8abe02a65efe3bf0bce3fddfc3
shbhmj <35397465+shbhmj@users.noreply.github.com>
2021-01-08 00:22:03 +0530

    Update register.php

--- register.php
+++ register.php
@@ -10,7 +10,7 @@ $errors = array();
 $db = mysqli_connect('localhost', 'dbmasteruser', '6KYY>nYf|TW}Jz5f|K#5DDIKE6B*bxm#', 'dbmaster');
 
 // REGISTER USER
-if (isset($_POST['reg_user'])) {
+if(isset($_POST['reg_user'])) {
   // receive all input values from the form
   $name = mysqli_real_escape_string($db, $_POST['name']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
@@ -19,10 +19,10 @@ if (isset($_POST['reg_user'])) {
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
-  if (empty($username)) { array_push($errors, "Username is required"); }
-  if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
-  if ($password_1 != $password_2) {
+  if(empty($username)) { array_push($errors, "Username is required"); }
+  if(empty($email)) { array_push($errors, "Email is required"); }
+  if(empty($password_1)) { array_push($errors, "Password is required"); }
+  if($password_1 != $password_2) {
 	array_push($errors, "The two passwords do not match");
   }
 
@@ -32,14 +32,14 @@ if (isset($_POST['reg_user'])) {
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
-  if ($user) { // if user exists
-    if ($user['email'] === $email) {
+  if($user) { // if user exists
+    if($user['email'] === $email) {
       array_push($errors, "email already exists");
     }
   }
 
   // Finally, register user if there are no errors in the form
-  if (count($errors) == 0) {
+  if(count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
 
   	$query = "INSERT INTO users (name, email, password) 
@@ -47,6 +47,6 @@ if (isset($_POST['reg_user'])) {
   	mysqli_query($db, $query);
   	$_SESSION['name'] = $name;
   	$_SESSION['success'] = "You are now logged in";
-  	header('location: login.php');
+  //	header('location: login.php');
   }
 }
